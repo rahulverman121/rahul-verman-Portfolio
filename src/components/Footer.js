ae5c06713431bf67716ce9fb6df450b98df2613d
rahulverman121 <98263712+rahulverman121@users.noreply.github.com>
2022-10-07 18:07:59 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+    return(
+       <section className="footer">
+        <div>&copy; Designed &amp; Developed By <i>RAHUL VERMAN</i></div>
+        <div>INDIA | 2022</div>
+       </section>
+    )
+}
+export default Footer;
